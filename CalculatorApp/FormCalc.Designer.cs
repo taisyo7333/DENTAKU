150a4bb778503b72c10a117e4b2ee68cc7dc8702
ds-inoue <taisyo9170@yahoo.co.jp>
2015-10-19 21:54:44 +0900

    Disable Tabstop except '=' .
    This is because when a user pres key 'enter' , she expects this app show her the result of calculation as same as press '='.

--- CalculatorApp/FormCalc.Designer.cs
+++ CalculatorApp/FormCalc.Designer.cs
@@ -65,6 +65,7 @@
             this.buttonBackSpace.Name = "buttonBackSpace";
             this.buttonBackSpace.Size = new System.Drawing.Size(54, 37);
             this.buttonBackSpace.TabIndex = 0;
+            this.buttonBackSpace.TabStop = false;
             this.buttonBackSpace.Text = "←";
             this.buttonBackSpace.UseVisualStyleBackColor = true;
             this.buttonBackSpace.Click += new System.EventHandler(this.buttonBackSpace_Click);
@@ -75,6 +76,7 @@
             this.buttonClearEdit.Name = "buttonClearEdit";
             this.buttonClearEdit.Size = new System.Drawing.Size(54, 37);
             this.buttonClearEdit.TabIndex = 1;
+            this.buttonClearEdit.TabStop = false;
             this.buttonClearEdit.Text = "CE";
             this.buttonClearEdit.UseVisualStyleBackColor = true;
             this.buttonClearEdit.Click += new System.EventHandler(this.buttonClearEdit_Click);
@@ -85,6 +87,7 @@
             this.buttonAllClear.Name = "buttonAllClear";
             this.buttonAllClear.Size = new System.Drawing.Size(54, 37);
             this.buttonAllClear.TabIndex = 2;
+            this.buttonAllClear.TabStop = false;
             this.buttonAllClear.Text = "C";
             this.buttonAllClear.UseVisualStyleBackColor = true;
             this.buttonAllClear.Click += new System.EventHandler(this.buttonAllClear_Click);
@@ -95,6 +98,7 @@
             this.buttonInputSign.Name = "buttonInputSign";
             this.buttonInputSign.Size = new System.Drawing.Size(54, 37);
             this.buttonInputSign.TabIndex = 3;
+            this.buttonInputSign.TabStop = false;
             this.buttonInputSign.Text = "±";
             this.buttonInputSign.UseVisualStyleBackColor = true;
             this.buttonInputSign.Click += new System.EventHandler(this.buttonInputSign_Click);
@@ -105,6 +109,7 @@
             this.buttonInput7.Name = "buttonInput7";
             this.buttonInput7.Size = new System.Drawing.Size(54, 37);
             this.buttonInput7.TabIndex = 4;
+            this.buttonInput7.TabStop = false;
             this.buttonInput7.Text = "７";
             this.buttonInput7.UseVisualStyleBackColor = true;
             this.buttonInput7.Click += new System.EventHandler(this.buttonInput7_Click);
@@ -115,6 +120,7 @@
             this.buttonInput8.Name = "buttonInput8";
             this.buttonInput8.Size = new System.Drawing.Size(54, 37);
             this.buttonInput8.TabIndex = 5;
+            this.buttonInput8.TabStop = false;
             this.buttonInput8.Text = "８";
             this.buttonInput8.UseVisualStyleBackColor = true;
             this.buttonInput8.Click += new System.EventHandler(this.buttonInput8_Click);
@@ -125,6 +131,7 @@
             this.buttonInput9.Name = "buttonInput9";
             this.buttonInput9.Size = new System.Drawing.Size(54, 37);
             this.buttonInput9.TabIndex = 6;
+            this.buttonInput9.TabStop = false;
             this.buttonInput9.Text = "９";
             this.buttonInput9.UseVisualStyleBackColor = true;
             this.buttonInput9.Click += new System.EventHandler(this.buttonInput9_Click);
@@ -135,6 +142,7 @@
             this.buttonInputDiv.Name = "buttonInputDiv";
             this.buttonInputDiv.Size = new System.Drawing.Size(54, 37);
             this.buttonInputDiv.TabIndex = 7;
+            this.buttonInputDiv.TabStop = false;
             this.buttonInputDiv.Text = "／";
             this.buttonInputDiv.UseVisualStyleBackColor = true;
             this.buttonInputDiv.Click += new System.EventHandler(this.buttonInputDiv_Click);
@@ -145,6 +153,7 @@
             this.buttonInput4.Name = "buttonInput4";
             this.buttonInput4.Size = new System.Drawing.Size(54, 37);
             this.buttonInput4.TabIndex = 8;
+            this.buttonInput4.TabStop = false;
             this.buttonInput4.Text = "４";
             this.buttonInput4.UseVisualStyleBackColor = true;
             this.buttonInput4.Click += new System.EventHandler(this.buttonInput4_Click);
@@ -155,6 +164,7 @@
             this.buttonInput5.Name = "buttonInput5";
             this.buttonInput5.Size = new System.Drawing.Size(54, 37);
             this.buttonInput5.TabIndex = 9;
+            this.buttonInput5.TabStop = false;
             this.buttonInput5.Text = "５";
             this.buttonInput5.UseVisualStyleBackColor = true;
             this.buttonInput5.Click += new System.EventHandler(this.buttonInput5_Click);
@@ -165,6 +175,7 @@
             this.buttonInput6.Name = "buttonInput6";
             this.buttonInput6.Size = new System.Drawing.Size(54, 37);
             this.buttonInput6.TabIndex = 10;
+            this.buttonInput6.TabStop = false;
             this.buttonInput6.Text = "６";
             this.buttonInput6.UseVisualStyleBackColor = true;
             this.buttonInput6.Click += new System.EventHandler(this.buttonInput6_Click);
@@ -175,6 +186,7 @@
             this.buttonInput1.Name = "buttonInput1";
             this.buttonInput1.Size = new System.Drawing.Size(54, 37);
             this.buttonInput1.TabIndex = 11;
+            this.buttonInput1.TabStop = false;
             this.buttonInput1.Text = "１";
             this.buttonInput1.UseVisualStyleBackColor = true;
             this.buttonInput1.Click += new System.EventHandler(this.buttonInput1_Click);
@@ -185,6 +197,7 @@
             this.buttonInput2.Name = "buttonInput2";
             this.buttonInput2.Size = new System.Drawing.Size(54, 37);
             this.buttonInput2.TabIndex = 12;
+            this.buttonInput2.TabStop = false;
             this.buttonInput2.Text = "２";
             this.buttonInput2.UseVisualStyleBackColor = true;
             this.buttonInput2.Click += new System.EventHandler(this.buttonInput2_Click);
@@ -195,6 +208,7 @@
             this.buttonInput3.Name = "buttonInput3";
             this.buttonInput3.Size = new System.Drawing.Size(54, 37);
             this.buttonInput3.TabIndex = 13;
+            this.buttonInput3.TabStop = false;
             this.buttonInput3.Text = "３";
             this.buttonInput3.UseVisualStyleBackColor = true;
             this.buttonInput3.Click += new System.EventHandler(this.buttonInput3_Click);
@@ -205,6 +219,7 @@
             this.buttonPoint.Name = "buttonPoint";
             this.buttonPoint.Size = new System.Drawing.Size(54, 37);
             this.buttonPoint.TabIndex = 14;
+            this.buttonPoint.TabStop = false;
             this.buttonPoint.Text = "．";
             this.buttonPoint.UseVisualStyleBackColor = true;
             this.buttonPoint.Visible = false;
@@ -216,6 +231,7 @@
             this.buttonInputMul.Name = "buttonInputMul";
             this.buttonInputMul.Size = new System.Drawing.Size(54, 37);
             this.buttonInputMul.TabIndex = 15;
+            this.buttonInputMul.TabStop = false;
             this.buttonInputMul.Text = "＊";
             this.buttonInputMul.UseVisualStyleBackColor = true;
             this.buttonInputMul.Click += new System.EventHandler(this.buttonInputMul_Click);
@@ -226,6 +242,7 @@
             this.buttonInputSub.Name = "buttonInputSub";
             this.buttonInputSub.Size = new System.Drawing.Size(54, 37);
             this.buttonInputSub.TabIndex = 16;
+            this.buttonInputSub.TabStop = false;
             this.buttonInputSub.Text = "-";
             this.buttonInputSub.UseVisualStyleBackColor = true;
             this.buttonInputSub.Click += new System.EventHandler(this.buttonInputSub_Click);
@@ -236,6 +253,7 @@
             this.buttonInputAdd.Name = "buttonInputAdd";
             this.buttonInputAdd.Size = new System.Drawing.Size(54, 37);
             this.buttonInputAdd.TabIndex = 17;
+            this.buttonInputAdd.TabStop = false;
             this.buttonInputAdd.Text = "+";
             this.buttonInputAdd.UseVisualStyleBackColor = true;
             this.buttonInputAdd.Click += new System.EventHandler(this.buttonInputAdd_Click);
@@ -246,6 +264,7 @@
             this.buttonInput0.Name = "buttonInput0";
             this.buttonInput0.Size = new System.Drawing.Size(114, 37);
             this.buttonInput0.TabIndex = 18;
+            this.buttonInput0.TabStop = false;
             this.buttonInput0.Text = "0";
             this.buttonInput0.UseVisualStyleBackColor = true;
             this.buttonInput0.Click += new System.EventHandler(this.buttonInput0_Click);
