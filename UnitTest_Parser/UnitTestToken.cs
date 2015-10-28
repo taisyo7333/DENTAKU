bc206c80168dfbf1e0586ea4736b93751a0e2103
ds-inoue <taisyo9170@yahoo.co.jp>
2015-10-29 06:18:14 +0900

    Modified method's attribute to have static because the methods have been used as utility.

--- UnitTest_Parser/UnitTestToken.cs
+++ UnitTest_Parser/UnitTestToken.cs
@@ -11,63 +11,56 @@ namespace UnitTest_Parser
         [TestMethod]
         public void TestMethod_isOperator()
         {
-            Token a = new Token();
-            Assert.AreEqual(true, a.isOperator('+'));
-            Assert.AreEqual(true, a.isOperator('-'));
-            Assert.AreEqual(true, a.isOperator('*'));
-            Assert.AreEqual(true, a.isOperator('/'));
+            Assert.AreEqual(true, Token.isOperator('+'));
+            Assert.AreEqual(true, Token.isOperator('-'));
+            Assert.AreEqual(true, Token.isOperator('*'));
+            Assert.AreEqual(true, Token.isOperator('/'));
         }
         [TestMethod]
         public void TestMethod_isParenthesis()
         {
-            Token a = new Token();
-            Assert.AreEqual(true, a.isParenthesis('('));
-            Assert.AreEqual(true, a.isParenthesis(')'));
+            Assert.AreEqual(true, Token.isParenthesis('('));
+            Assert.AreEqual(true, Token.isParenthesis(')'));
         }
         [TestMethod]
         public void TestMethod_isNumber()
         {
-            Token a = new Token();
-            Assert.AreEqual(true, a.isNumber('0'));
-            Assert.AreEqual(true, a.isNumber('1'));
-            Assert.AreEqual(true, a.isNumber('2'));
-            Assert.AreEqual(true, a.isNumber('3'));
-            Assert.AreEqual(true, a.isNumber('4'));
-            Assert.AreEqual(true, a.isNumber('5'));
-            Assert.AreEqual(true, a.isNumber('6'));
-            Assert.AreEqual(true, a.isNumber('7'));
-            Assert.AreEqual(true, a.isNumber('8'));
-            Assert.AreEqual(true, a.isNumber('9'));
-            Assert.AreEqual(false, a.isNumber('a'));
-            Assert.AreEqual(false, a.isNumber('b'));
-            Assert.AreEqual(false, a.isNumber('c'));
-            Assert.AreEqual(false, a.isNumber('d'));
-            Assert.AreEqual(false, a.isNumber('e'));
-            Assert.AreEqual(false, a.isNumber('f'));
+            Assert.AreEqual(true, Token.isNumber('0'));
+            Assert.AreEqual(true, Token.isNumber('1'));
+            Assert.AreEqual(true, Token.isNumber('2'));
+            Assert.AreEqual(true, Token.isNumber('3'));
+            Assert.AreEqual(true, Token.isNumber('4'));
+            Assert.AreEqual(true, Token.isNumber('5'));
+            Assert.AreEqual(true, Token.isNumber('6'));
+            Assert.AreEqual(true, Token.isNumber('7'));
+            Assert.AreEqual(true, Token.isNumber('8'));
+            Assert.AreEqual(true, Token.isNumber('9'));
+            Assert.AreEqual(false, Token.isNumber('a'));
+            Assert.AreEqual(false, Token.isNumber('b'));
+            Assert.AreEqual(false, Token.isNumber('c'));
+            Assert.AreEqual(false, Token.isNumber('d'));
+            Assert.AreEqual(false, Token.isNumber('e'));
+            Assert.AreEqual(false, Token.isNumber('f'));
         }
         [TestMethod]
         public void TestMethod_isMinusSign()
         {
-            Token a = new Token();
-            Assert.AreEqual(true, a.isMinusSign('-'));
-            Assert.AreEqual(false, a.isMinusSign('+'));
+            Assert.AreEqual(true, Token.isMinusSign('-'));
+            Assert.AreEqual(false, Token.isMinusSign('+'));
         }
         [TestMethod]
         public void TestMethod_isEqual()
         {
-            Token a = new Token();
-            Assert.AreEqual(true, a.isEqual('='));
+            Assert.AreEqual(true, Token.isEqual('='));
         }
         [TestMethod]
         public void TestMethod_isBackSpace()
         {
-            Token a = new Token();
-            Assert.AreEqual(true, a.isBackSpace((char)Keys.Back));
+            Assert.AreEqual(true, Token.isBackSpace((char)Keys.Back));
         }
         [TestMethod]
         public void TestMethod_isToken()
         {
-            Token a = new Token();
             char[] tokens = new char[]{
                 '0','1','2','3','4','5','6','7','8','9',
                 '+','-','*','/',
@@ -75,9 +68,9 @@ namespace UnitTest_Parser
             };
             foreach(var v in tokens)
             {
-                Assert.AreEqual(true,a.isToken(v));
+                Assert.AreEqual(true,Token.isToken(v));
             }
-            Assert.AreEqual(false, a.isToken('='));
+            Assert.AreEqual(false, Token.isToken('='));
         }
 
     }
